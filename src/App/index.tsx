f6ae9cb377227517ee350ddb664c0dd39d885b15
Jonas Oliveira <jonasfissicaro13@gmail.com>
2021-08-19 21:48:32 -0300

    add styled components and refactor App

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,12 +1,21 @@
 import React from "react";
-import Header from "../components/Header";
-import "./App.scss";
+import styled from "styled-components";
+import GameBoard from "./GameBoard";
+import Header from "./Header";
+
+const StyledApp = styled.main`
+  background-image: radial-gradient(hsl(214, 47%, 23%), hsl(237, 49%, 15%));
+  min-width: 100vw;
+  min-height: 100vh;
+  padding: 3rem;
+`;
 
 function App() {
   return (
-    <main className="App">
+    <StyledApp>
       <Header />
-    </main>
+      <GameBoard />
+    </StyledApp>
   );
 }
 
