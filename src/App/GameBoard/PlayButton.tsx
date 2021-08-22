9068da0a62621b40e8ed21e7ad1fdf3bdb404fd7
Jonas Oliveira <jonasfissicaro13@gmail.com>
2021-08-22 15:38:06 -0300

    create button attributes

--- src/App/GameBoard/PlayButton.tsx
+++ src/App/GameBoard/PlayButton.tsx
@@ -6,6 +6,36 @@ const boxShadow = (color: string): string => {
   return `inset 0 4px 0px 0px #b4b4b4, 0px 5px ${color}`;
 };
 
+const playButtonAttributes = {
+  [PlayButtonType.PAPER]: {
+    style: css`
+      border-color: var(--paperGradient2);
+      box-shadow: ${boxShadow("var(--paperGradient1)")};
+      grid-area: paperButton;
+    `,
+    imageLink: "icon-paper.svg",
+    imageAlt: "paper play button",
+  },
+  [PlayButtonType.ROCK]: {
+    style: css`
+      border-color: var(--rockGradient2);
+      box-shadow: ${boxShadow("var(--rockGradient1)")};
+      grid-area: rockButton;
+    `,
+    imageLink: "icon-rock.svg",
+    imageAlt: "rock play button",
+  },
+  [PlayButtonType.SCISSORS]: {
+    style: css`
+      border-color: var(--scissorsGradient2);
+      box-shadow: ${boxShadow("var(--scissorsGradient1)")};
+      grid-area: scissorsButton;
+    `,
+    imageLink: "icon-scissors.svg",
+    imageAlt: "scissors play button",
+  },
+};
+
 const StyledPlayButton = styled.button<IProps>`
   width: ${"clamp(8.5rem, 10vw, 12rem)"};
   height: ${"clamp(8.5rem, 10vw, 12rem)"};
@@ -24,27 +54,7 @@ const StyledPlayButton = styled.button<IProps>`
   }
 
   ${(props) => {
-    if (props.buttonType === PlayButtonType.PAPER) {
-      return css`
-        border-color: var(--paperGradient2);
-        box-shadow: ${boxShadow("var(--paperGradient1)")};
-        grid-area: paperButton;
-      `;
-    }
-    if (props.buttonType === PlayButtonType.ROCK) {
-      return css`
-        border-color: var(--rockGradient2);
-        box-shadow: ${boxShadow("var(--rockGradient1)")};
-        grid-area: rockButton;
-      `;
-    }
-    if (props.buttonType === PlayButtonType.SCISSORS) {
-      return css`
-        border-color: var(--scissorsGradient2);
-        box-shadow: ${boxShadow("var(--scissorsGradient1)")};
-        grid-area: scissorsButton;
-      `;
-    }
+    return playButtonAttributes[props.buttonType].style;
   }}
 `;
 
@@ -53,22 +63,11 @@ interface IProps {
 }
 
 const PlayButton = ({ buttonType }: IProps) => {
-  function getImageAtributes(): { link: string; alt: string } {
-    switch (buttonType) {
-      case PlayButtonType.PAPER:
-        return { link: "icon-paper.svg", alt: "paper play button" };
-      case PlayButtonType.ROCK:
-        return { link: "icon-rock.svg", alt: "rock play button" };
-      case PlayButtonType.SCISSORS:
-        return { link: "icon-scissors.svg", alt: "scissors play button" };
-    }
-  }
-
   return (
     <StyledPlayButton {...{ buttonType }}>
       <img
-        src={`./img/${getImageAtributes().link}`}
-        alt={getImageAtributes().alt}
+        src={`./img/${playButtonAttributes[buttonType].imageLink}`}
+        alt={playButtonAttributes[buttonType].imageAlt}
       />
     </StyledPlayButton>
   );
