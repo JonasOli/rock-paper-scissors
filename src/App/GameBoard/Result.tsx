9f012913a7b6d1f3041ec9f56af5213cc9762361
Jonas Oliveira <jonasfissicaro13@gmail.com>
2022-01-21 20:56:38 -0300

    add styles to the result page

--- src/App/GameBoard/Result.tsx
+++ src/App/GameBoard/Result.tsx
@@ -1,7 +1,8 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext } from "react";
 import { animated } from "react-spring";
 import styled from "styled-components";
 import PlayButtonType from "../../Helper/PlayButtonType";
+import Winner from "../../Helper/Winner";
 import { AppContext } from "../Context/AppContext";
 import PlayButton from "./PlayButton";
 
@@ -10,6 +11,8 @@ const StyledResultWrapper = styled(animated.div)`
   align-items: center;
   justify-content: center;
   margin: auto 0;
+  gap: 0 3rem;
+  position: relative;
 `;
 
 const Picks = styled.div`
@@ -19,25 +22,63 @@ const Picks = styled.div`
 
   h2 {
     color: white;
-    font-size: 2rem;
-    margin-bottom: 2rem;
+    text-transform: uppercase;
+    font-size: clamp(1rem, 2.5vw, 1.5rem);
+    margin: 0;
+    margin-top: 2rem;
+    letter-spacing: 2px;
+  }
+
+  @media only screen and (min-width: 48em) {
+    flex-direction: column-reverse;
+
+    h2 {
+      margin: 0;
+      margin-bottom: 2rem;
+    }
+  }
+`;
+
+const ResultsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 2rem 0;
+  margin: 0 clamp(0, 2.5vw, 3rem);
+  position: absolute;
+  top: calc(100% + 7rem);
+
+  h2 {
+    color: #fff;
+    text-transform: uppercase;
+    text-align: center;
+    line-height: 0.8;
+    letter-spacing: 2px;
+    font-size: 4rem;
+  }
+
+  @media only screen and (min-width: 48em) {
+    position: relative;
   }
 `;
 
 const PlayAgainButton = styled.button`
   margin-top: auto;
+  width: 15rem;
+  padding: 0.5rem 0;
+  border-radius: 5px;
+  font-size: clamp(1.5rem, 2.5vw, 1.5rem);
+  border: none;
+  color: var(--darkText);
+  letter-spacing: 1px;
+  text-transform: uppercase;
+  cursor: pointer;
 `;
 
 const Result = ({ style }: { style: any }) => {
-  const { setShowResult, yourPick, pcPick, score, setScore } =
-    useContext(AppContext);
-  const [resultText, setResultText] = useState("");
+  const { yourPick, pcPick, winner, resetValues } = useContext(AppContext);
 
-  useEffect(() => {
-    checkResult();
-  }, []);
-
-  const renderPick = (buttonType: PlayButtonType) => {
+  function renderPick(buttonType: PlayButtonType | null): JSX.Element {
     switch (buttonType) {
       case PlayButtonType.PAPER:
         return <PlayButton buttonType={PlayButtonType.PAPER} />;
@@ -45,47 +86,45 @@ const Result = ({ style }: { style: any }) => {
         return <PlayButton buttonType={PlayButtonType.ROCK} />;
       case PlayButtonType.SCISSORS:
         return <PlayButton buttonType={PlayButtonType.SCISSORS} />;
+      default:
+        return <div>empty</div>;
     }
-  };
+  }
 
-  const checkResult = () => {
-    if (yourPick === pcPick) {
-      setResultText("Draw");
-    } else if (
-      (yourPick === PlayButtonType.PAPER &&
-        pcPick === PlayButtonType.SCISSORS) ||
-      (yourPick === PlayButtonType.ROCK && pcPick === PlayButtonType.PAPER) ||
-      (yourPick === PlayButtonType.SCISSORS && pcPick === PlayButtonType.ROCK)
-    ) {
-      setResultText("You lose");
-    } else if (
-      (yourPick === PlayButtonType.PAPER && pcPick === PlayButtonType.ROCK) ||
-      (yourPick === PlayButtonType.ROCK &&
-        pcPick === PlayButtonType.SCISSORS) ||
-      (yourPick === PlayButtonType.SCISSORS && pcPick === PlayButtonType.PAPER)
-    ) {
-      setScore(score + 1);
-      setResultText("You win");
+  function getResultText(): string | undefined {
+    switch (winner) {
+      case Winner.PLAYER:
+        return "You won";
+      case Winner.MACHINE:
+        return "You lose";
+      case Winner.DRAW:
+        return "Draw";
     }
-  };
+  }
 
   return (
     <StyledResultWrapper style={style}>
       <Picks>
-        <h2>Your picked</h2>
         {renderPick(yourPick)}
+        <h2>Your picked</h2>
       </Picks>
 
-      <div>
-        <h2>{resultText}</h2>
-        <PlayAgainButton onClick={() => setShowResult(false)}>
-          Play again
-        </PlayAgainButton>
-      </div>
+      {!!winner && (
+        <ResultsContainer>
+          <h2>{getResultText()}</h2>
+          <PlayAgainButton
+            onClick={() => {
+              resetValues();
+            }}
+          >
+            Play again
+          </PlayAgainButton>
+        </ResultsContainer>
+      )}
 
       <Picks>
-        <h2>The house picked</h2>
         {renderPick(pcPick)}
+        <h2>The house picked</h2>
       </Picks>
     </StyledResultWrapper>
   );
