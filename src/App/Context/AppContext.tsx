fb4f8a9104713a560ceabdbe1d1a7a712a5f82da
Jonas Oliveira <jonasfissicaro13@gmail.com>
2022-01-21 20:55:51 -0300

    simplify the app context

--- src/App/Context/AppContext.tsx
+++ src/App/Context/AppContext.tsx
@@ -1,46 +1,92 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import PlayButtonType from "../../Helper/PlayButtonType";
+import Winner from "../../Helper/Winner";
 
 interface IContext {
   showResult: boolean;
-  setShowResult: (togge: boolean) => void;
   score: number;
-  setScore: (score: number) => void;
+  yourPick: PlayButtonType | null;
+  pcPick: PlayButtonType | null;
+  winner: Winner | null;
   play: (choice: PlayButtonType) => void;
-  yourPick: PlayButtonType;
-  pcPick: PlayButtonType;
+  resetValues: () => void;
 }
 
 export const AppContext = createContext<IContext>({
   score: 0,
   showResult: false,
-  setShowResult: () => {},
-  setScore: () => {},
+  yourPick: null,
+  pcPick: null,
+  winner: null,
   play: () => {},
-  yourPick: 0,
-  pcPick: 0
+  resetValues: () => {},
 });
 
 const AppProvider = ({ children }: { children: any }) => {
   const [showResult, setShowResult] = useState<boolean>(false);
   const [score, setScore] = useState<number>(0);
-  const [yourPick, setYourPick] = useState<PlayButtonType>(0);
-  const [pcPick, setPcPick] = useState<PlayButtonType>(0);
+  const [yourPick, setYourPick] = useState<PlayButtonType | null>(0);
+  const [pcPick, setPcPick] = useState<PlayButtonType | null>(null);
+  const [winner, setWinner] = useState<Winner | null>(null);
 
-  const play = (choice: PlayButtonType): void => {
+  useEffect(() => {
+    if (!!pcPick) checkResult();
+  }, [pcPick]);
+
+  function play(choice: PlayButtonType): void {
     const randomChoiceByPC = randomIntFromInterval(0, 2);
 
     setYourPick(choice);
-    setPcPick(randomChoiceByPC)
     setShowResult(true);
-  };
+    setTimeout(() => {
+      setPcPick(randomChoiceByPC);
+    }, 2000);
+  }
+
+  function resetValues() {
+    setYourPick(null);
+    setPcPick(null);
+    setWinner(null);
+    setShowResult(false);
+  }
+
+  function checkResult() {
+    if (yourPick === pcPick) {
+      setWinner(Winner.DRAW);
+    } else if (
+      (yourPick === PlayButtonType.PAPER &&
+        pcPick === PlayButtonType.SCISSORS) ||
+      (yourPick === PlayButtonType.ROCK && pcPick === PlayButtonType.PAPER) ||
+      (yourPick === PlayButtonType.SCISSORS && pcPick === PlayButtonType.ROCK)
+    ) {
+      setWinner(Winner.MACHINE);
+    } else if (
+      (yourPick === PlayButtonType.PAPER && pcPick === PlayButtonType.ROCK) ||
+      (yourPick === PlayButtonType.ROCK &&
+        pcPick === PlayButtonType.SCISSORS) ||
+      (yourPick === PlayButtonType.SCISSORS && pcPick === PlayButtonType.PAPER)
+    ) {
+      setScore(score + 1);
+      setWinner(Winner.PLAYER);
+    }
+  }
 
   function randomIntFromInterval(min: number, max: number): number {
     return Math.floor(Math.random() * (max - min + 1) + min);
   }
 
   return (
-    <AppContext.Provider value={{ showResult, setShowResult, score, setScore, play, yourPick, pcPick }}>
+    <AppContext.Provider
+      value={{
+        showResult,
+        score,
+        yourPick,
+        pcPick,
+        winner,
+        play,
+        resetValues,
+      }}
+    >
       {children}
     </AppContext.Provider>
   );
